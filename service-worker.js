909b41ef656856d924171e6f14cade7483eb6053
edsoneoaalves-del <edson.eoa.alves@gmail.com>
2026-08-07 15:25:35 -0300

    XCMG_Control_v6.0.9

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='xcmg-control-v6.0.9-mobile-compact-1';
-const APP_SHELL=['./','./index.html','./css/style.css','./js/app.js','./manifest.json','./icons/icon-192.png','./icons/icon-512.png','./modelo_colaboradores.csv'];
+const CACHE='xcmg-control-icon-v6.0.11';
+const APP_SHELL=['./','./index.html','./css/style.css','./js/app.js','./manifest.json','./icons/icon-192-v6011.png','./icons/icon-512-v6011.png','./modelo_colaboradores.csv'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(APP_SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;const url=new URL(event.request.url);if(url.origin!==self.location.origin)return;if(event.request.mode==='navigate'){event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put('./index.html',copy));return response}).catch(()=>caches.match('./index.html')));return}event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{if(response&&response.ok){const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy))}return response}))) });
